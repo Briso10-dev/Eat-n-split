85c04dd0372a3473c3a38d9f53a919d024e0d35e
Briso10-dev <kouambrice10@gmail.com>
2025-07-03 21:27:14 +0200

    Adding tailwind.config.js file for colors styling-setting back vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        lightest: '#fff4e6',
-        light: '#ffe8cc',
-        medium: '#ffa94d',
-        dark: '#ff922b',
-      }
-    }
-  },
   plugins: [
     react(),
     tailwindcss()
