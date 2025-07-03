22a4af0a1bb10ace701e406d20416c6f9dae3397
Briso10-dev <kouambrice10@gmail.com>
2025-07-03 20:54:12 +0200

    Styling go to start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,9 +24,9 @@ const initialFriends = [
 function App() {
 
   return (
-    <>
+    <div className="min-h-[66vh] grid grid-cols-[34rem_44rem] gap-x-[4rem] items-star">
       <FriendList/>
-    </>
+    </div>
   )
 }
 
