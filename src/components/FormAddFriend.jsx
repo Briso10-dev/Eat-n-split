36ec55dee3ebdbb66cb4c7e6a318dcd0616fd8a4
Briso10-dev <kouambrice10@gmail.com>
2025-07-04 19:30:03 +0200

    FormAddFriend static component created

---
+++ src/components/FormAddFriend.jsx
@@ -0,0 +1,16 @@
+
+function FormAddFriend(){
+    return(
+        <form className="">
+            <label>Friend name</label>
+            <input type="text" />
+
+            <label htmlFor="">Image URL</label>
+            <input type="text" />
+
+        </form>
+    )
+
+}
+
+export default FormAddFriend;
