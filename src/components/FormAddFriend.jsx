97d14bc83ca33296eb8128021d8cc7bb4a0244e2
Briso10-dev <kouambrice10@gmail.com>
2025-07-05 22:55:50 +0200

    Correction Styling 3

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -2,15 +2,15 @@ import Button from "./Button";
 
 function FormAddFriend() {
     return (
-        <form className="h-[15rem] text-[1.8rem] flex flex-col items-cente gap-[1.2rem] bg-[#fff4e6] rounded-[1rem] p-[2rem]">
-            <div className="flex gap-10">
-                <label className="self-start px-[1rem]">Friend name</label>
+        <form className="w-[50rem] h-[15rem]  text-[1.8rem] flex flex-col items-cente gap-[1.2rem] bg-[#fff4e6] rounded-[1rem] p-[2rem]">
+            <div className="flex gap-[5rem]">
+                <label className="self-start">Friend name</label>
                 <input
                     type="text"
                     className="w-[30rem] h-[3rem] text-[1.4rem] p-[1rem] border border-[#ffa94d] bg-gray-200 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-[#ffa94d]"
                 />
             </div>
-            <div className="flex gap-10">
+            <div className="flex gap-[5rem]">
                 <label className="self-start">Image URL</label>
                 <input
                     type="text"
