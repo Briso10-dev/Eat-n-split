d247d823c9cb09be61572e626ec17a2f661cd863
Briso10-dev <kouambrice10@gmail.com>
2025-07-05 22:49:27 +0200

    Correct styling 2 of form commponent

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="min-h-[66vh] grid grid-cols-[34rem_44rem] gap-x-[4rem] items-start">
-      <div className="flex flex-col gap-6 w-[45rem]">
+      <div className="flex flex-col gap-10 w-[45rem]">
         <FriendList/>
         <FormAddFriend />
         <div className="self-end"><Button>Add friend</Button></div>
--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -1,23 +1,25 @@
 import Button from "./Button";
 
 function FormAddFriend() {
-  return (
-    <form className="text-[1.8rem] flex flex-col items-center gap-[1.2rem] bg-[#fff4e6] rounded-[1rem] p-[2rem]">
-      <label className="self-start">Friend name</label>
-      <input
-        type="text"
-        className="w-full h-[3rem] text-[1.4rem] p-[1rem] border border-[#ffa94d] bg-gray-200 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-[#ffa94d]"
-      />
-
-      <label className="self-start">Image URL</label>
-      <input
-        type="text"
-         className="w-full h-[3rem] text-[1.4rem] p-[1rem] border border-[#ffa94d] bg-gray-200 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-[#ffa94d]"
-      />
-
-      <Button>Add</Button>
-    </form>
-  );
+    return (
+        <form className="h-[15rem] text-[1.8rem] flex flex-col items-cente gap-[1.2rem] bg-[#fff4e6] rounded-[1rem] p-[2rem]">
+            <div className="flex gap-10">
+                <label className="self-start px-[1rem]">Friend name</label>
+                <input
+                    type="text"
+                    className="w-[30rem] h-[3rem] text-[1.4rem] p-[1rem] border border-[#ffa94d] bg-gray-200 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-[#ffa94d]"
+                />
+            </div>
+            <div className="flex gap-10">
+                <label className="self-start">Image URL</label>
+                <input
+                    type="text"
+                    className="w-[30rem] h-[3rem] text-[1.4rem] p-[1rem] border border-[#ffa94d] bg-gray-200 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-[#ffa94d]"
+                />
+            </div>
+            <div className="self-center"><Button>Add</Button></div>
+        </form>
+    );
 }
 
 export default FormAddFriend;
