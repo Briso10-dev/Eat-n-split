78c230086c76a03c255b449435bf3e642a849051
Briso10-dev <kouambrice10@gmail.com>
2025-07-08 01:11:01 +0200

    Definding state for the feature of Adding friend

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Button from "./Button";
 
 function FormAddFriend() {
+
+    const [name,setName] = useState("");
+    const [image, setImage] = useState("");
+
     return (
         <form className="w-[45rem] h-[15rem]  text-[1.8rem] flex flex-col items-cente gap-[1.2rem] bg-[#fff4e6] rounded-[1rem] p-[2rem]">
             <div className="flex gap-[5rem]">
