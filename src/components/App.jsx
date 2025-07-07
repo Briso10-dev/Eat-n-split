43277be5d8221c72cf2ec6fcf0fd6d51514fbfd6
Briso10-dev <kouambrice10@gmail.com>
2025-07-08 00:01:34 +0200

    A control element form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,13 @@ import FormSplitBill from "./FormSplitBill";
 function App() {
 
    const [showAddFriend, setShowAddFriend] = useState(false);
-    
+
 
   return (
     <div className="min-h-[66vh] grid grid-cols-[34rem_44rem] gap-x-[4rem] items-start">
       <div className="flex flex-col gap-10 w-[45rem]">
         <FriendList/>
-        <FormAddFriend />
+        {showAddFriend && <FormAddFriend />}
         <div className="self-end"><Button>Add friend</Button></div>
       </div>
 
