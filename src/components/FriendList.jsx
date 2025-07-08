f2526881e0735cbec7d821641a151b6ceacbf699
Briso10-dev <kouambrice10@gmail.com>
2025-07-08 18:29:35 +0200

    LIfting up friend state to parent component-default initial friends

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,28 +1,7 @@
 import Friend from "./Freind";
 
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
-
 function FriendList(){
-    const friends = initialFriends;
+   
 
     return(
         <ul className="list-none flex flex-col gap-[3.4rem] text-[1.8rem] mb-8 w-[40rem]">
