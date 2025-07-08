87239f947cf4dc4589a5c11ae98ea478a41ae10e
Briso10-dev <kouambrice10@gmail.com>
2025-07-08 22:21:36 +0200

    Updating the expenses of user and friend on Ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,7 +49,11 @@ function App() {
 
   }
   function handleSplitBill(value){
-        console.log(value);
+      console.log(value);
+
+        setFriends(friends =>friends.map(friend => 
+        friend.id === selectedFriend.id ? 
+        {...friend, balance: friend.balance + value} : friend))
   }
 
   return (
